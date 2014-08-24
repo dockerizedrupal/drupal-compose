7b57b5c7aa94e0ce51c272919095a7416bddbb08
Jürgen Viljaste <j.viljaste@gmail.com>
2014-08-24 19:03:51 +0300

    Update

--- build/app/dev.js
+++ build/app/dev.js
@@ -3,14 +3,10 @@
 var fs = require('fs');
 var program = require('commander');
 
-console.log(3);
-
 program
   .command('init')
   .action(function(context) {
-    console.log(4);
-
-    fs.createReadStream('./dev.yaml').pipe(fs.createWriteStream(context +'/dev.yaml'));
+    fs.createReadStream('./dev.yaml').pipe(fs.createWriteStream(context + '/dev.yaml'));
   });
 
 program.parse(process.argv);
