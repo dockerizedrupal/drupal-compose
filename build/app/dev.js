7aebd42a4e90ca8eb34c2a987fffc5ecf8af2a4c
Jürgen Viljaste <j.viljaste@gmail.com>
2014-08-28 17:57:25 +0300

    Update

--- build/app/dev.js
+++ build/app/dev.js
@@ -4,9 +4,13 @@ var program = require('commander');
 var YAML = require('yamljs');
 var jsonQuery = require('json-query');
 
+process.stdout.write(1);
+
 program
   .command('yaml')
   .action(function(query) {
+    process.stdout.write(2);
+
     process.stdout.write(jsonQuery(query, {
       data: YAML.load('/src/dev.yaml')
     }));
