f73c92c26d4cdb606aa19707f5eac0a41ebcd125
Jürgen Viljaste <j.viljaste@gmail.com>
2014-08-28 18:28:55 +0300

    Update

--- build/app/dev.js
+++ build/app/dev.js
@@ -1,17 +1,17 @@
 #!/usr/bin/env node
 
-//var program = require('commander');
+var program = require('commander');
 //var YAML = require('yamljs');
 //var jsonQuery = require('json-query');
 
-process.stdout.write(1);
+program
+  .command('init')
+  .action(function() {
+    exec('cp /app/dev.yaml /src/dev.yaml', function(err, stdout, stderr) {
+      if (err) {
+        throw err;
+      }
+    });
+  });
 
-//program
-//  .command('yaml')
-//  .action(function(query) {
-//    process.stdout.write(2);
-//
-//    process.stdout.write(YAML.load('/src/dev.yaml'));
-//  });
-//
-//program.parse(process.argv);
+program.parse(process.argv);
