5d4a3a03c81c5ad5d0144f9e66cee0bec2fd1667
Jürgen Viljaste <j.viljaste@gmail.com>
2014-09-12 22:40:27 +0300

    Update

--- dev.sh
+++ dev.sh
@@ -153,7 +153,7 @@ config() {
       image "${IMAGE}" destroy
     ;;
     get)
-      printf %s "$(dev config get "${2}")"
+      echo "$(dev config get "${2}")"
     ;;
     set)
       dev config set "${2}" "${3}"
@@ -168,7 +168,7 @@ dev() {
     config)
       case "${2}" in
         get)
-          printf %s "$(sudo docker run --net host --rm -i -t -a stdout "${IMAGE}" config get "${3}" 2> >(log_error))"
+          echo "$(sudo docker run --net host --rm -i -t -a stdout "${IMAGE}" config get "${3}" 2> >(log_error))"
         ;;
         set)
           sudo docker run --net host --rm -i -t -a stdout "${IMAGE}" config set "${3}" "${4}" > >(log) 2> >(log_error)
