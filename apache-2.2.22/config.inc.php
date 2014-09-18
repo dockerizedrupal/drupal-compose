ff4ce45ae92d0718ac14aa7cd12d000b5c8e3d5e
Jürgen Viljaste <j.viljaste@gmail.com>
2014-09-19 01:03:22 +0300

    Update

--- apache-2.2.22/config.inc.php
+++ apache-2.2.22/config.inc.php
@@ -9,22 +9,22 @@ $i++;
 $cfg['Servers'][$i]['auth_type'] = 'cookie';
 $cfg['Servers'][$i]['host'] = '127.0.0.1';
 $cfg['Servers'][$i]['connect_type'] = 'tcp';
-$cfg['Servers'][$i]['compress'] = false;
-$cfg['Servers'][$i]['AllowNoPassword'] = false;
+$cfg['Servers'][$i]['compress'] = TRUE;
+$cfg['Servers'][$i]['AllowNoPassword'] = FALSE;
 
 $cfg['LoginCookieValidity'] = 86400;
 $cfg['UploadDir'] = '';
 $cfg['SaveDir'] = '';
 $cfg['Lang'] = 'en';
 $cfg['MaxRows'] = 1000;
-$cfg['DisplayBinaryAsHex'] = false;
-$cfg['ProtectBinary'] = false;
+$cfg['DisplayBinaryAsHex'] = FALSE;
+$cfg['ProtectBinary'] = FALSE;
 $cfg['MaxDbList'] = 1000;
 $cfg['MaxTableList'] = 1000;
 $cfg['ExecTimeLimit'] = 300;
-$cfg['NavigationTreeEnableGrouping'] = false;
+$cfg['NavigationTreeEnableGrouping'] = FALSE;
 $cfg['MaxNavigationItems'] = 1000;
 $cfg['NavigationTreeDisplayItemFilterMinimum'] = 1;
 $cfg['NavigationTreeDisplayDbFilterMinimum'] = 1;
 
-$_REQUEST['display_blob'] = true;
+$_REQUEST['display_blob'] = TRUE;
