beb735fc91638db055fd3b51943a0e478cfd600a
Jürgen Viljaste <j.viljaste@gmail.com>
2014-09-24 18:48:16 +0300

    Update

--- build/etc/puppet/manifests/build.pp
+++ build/etc/puppet/manifests/build.pp
@@ -1,34 +1,34 @@
-class packages {
-  package {[
-      'build-essential',
-      'subversion'
-    ]:
-    ensure => present
-  }
-}
-
-class dev {
-  exec { 'mkdir /src':
-    path => ['/bin']
-  }
-
-  file { '/app':
-    ensure => present,
-    recurse => true,
-    source => '/tmp/build/app'
-  }
-
-  exec { '/bin/bash -l -c "cd /app && npm install"':
-    require => File['/app']
-  }
-
-  file { '/app/app.js':
-    ensure => present,
-    source => '/tmp/build/app/app.js',
-    mode => 755,
-    require => File['/app']
-  }
-}
+#class packages {
+#  package {[
+#      'build-essential',
+#      'subversion'
+#    ]:
+#    ensure => present
+#  }
+#}
+#
+#class dev {
+#  exec { 'mkdir /src':
+#    path => ['/bin']
+#  }
+#
+#  file { '/app':
+#    ensure => present,
+#    recurse => true,
+#    source => '/tmp/build/app'
+#  }
+#
+#  exec { '/bin/bash -l -c "cd /app && npm install"':
+#    require => File['/app']
+#  }
+#
+#  file { '/app/app.js':
+#    ensure => present,
+#    source => '/tmp/build/app/app.js',
+#    mode => 755,
+#    require => File['/app']
+#  }
+#}
 
 node default {
   file { '/run.sh':
@@ -37,13 +37,13 @@ node default {
     mode => 755
   }
 
-  include packages
-  include dev
-
-  Class['packages'] -> Class['dev']
-
-  exec { 'apt-get update':
-    path => ['/usr/bin'],
-    before => Class['packages']
-  }
+#  include packages
+#  include dev
+#
+#  Class['packages'] -> Class['dev']
+#
+#  exec { 'apt-get update':
+#    path => ['/usr/bin'],
+#    before => Class['packages']
+#  }
 }
