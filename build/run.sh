43c38f0bd023266e1076f94c98285a0a1e9cb8e3
Jürgen Viljaste <j.viljaste@gmail.com>
2014-10-04 13:50:47 +0300

    Update

--- build/run.sh
+++ build/run.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 
-if [ "${#}" -eq 0 ]; then
-  /usr/bin/supervisord
-
-  exit
-fi
-
 case "${1}" in
   install)
     TMP="$(mktemp -d)"
@@ -13,5 +7,8 @@ case "${1}" in
     git clone http://git.simpledrupalcloud.com/simpledrupalcloud/dev.git "${TMP}"
 
     cp "${TMP}/dev.sh" /usr/local/bin/dev
+  ;;
+  *)
+    /usr/bin/supervisord
   ;;
 esac
