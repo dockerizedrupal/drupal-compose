7d87f39d086005bd4f76b3995ec29095fe78dac8
Jürgen Viljaste <j.viljaste@gmail.com>
2014-10-19 11:57:48 +0300

    Update

--- build/var/www/core/vendor/composer/autoload_psr4.php
+++ build/var/www/core/vendor/composer/autoload_psr4.php
@@ -11,5 +11,4 @@ return array(
     'Drupal\\Driver\\' => array($baseDir . '/drivers/lib/Drupal/Driver'),
     'Drupal\\Core\\' => array($baseDir . '/core/lib/Drupal/Core'),
     'Drupal\\Component\\' => array($baseDir . '/core/lib/Drupal/Component'),
-    'Drupal\\AppConsole\\' => array($baseDir . '/modules/console/src'),
 );
