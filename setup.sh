218ab1bfd0c43d0a1e550ded1a1578977056ced4
Jürgen Viljaste <j.viljaste@gmail.com>
2014-12-06 22:28:02 +0200

    Update

--- setup.sh
+++ setup.sh
@@ -18,11 +18,11 @@ sudo apt-get update
 sudo apt-get install -y puppet
 
 # https://tickets.puppetlabs.com/browse/PUP-2566
-sed -i '/templatedir=\$confdir\/templates/d' /etc/puppet/puppet.conf
+sudo sed -i '/templatedir=\$confdir\/templates/d' /etc/puppet/puppet.conf
 
-puppet module install puppetlabs/stdlib
+sudo puppet module install puppetlabs/stdlib
 
-puppet apply --modulepath=setup/modules setup/setup.pp
+sudo puppet apply --modulepath=setup/modules setup/setup.pp
 
 #sudo apt-get install -y openssh-server
 #cat /dev/zero | ssh-keygen -b 4096 -t rsa -N ""
