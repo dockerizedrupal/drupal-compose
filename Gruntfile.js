dd3b17135367a0f36b24ac8106be03f7eaf86252
Jürgen Viljaste <j.viljaste@gmail.com>
2015-09-24 22:19:59 +0300

    Update

---
+++ Gruntfile.js
@@ -0,0 +1,90 @@
+var current_version = '1.0.13';
+var new_version = '1.0.14';
+
+module.exports = function(grunt) {
+  grunt.loadNpmTasks('grunt-replace');
+
+  grunt.initConfig({
+    replace: {
+      task1: {
+        options: {
+          patterns: [
+            {
+              match: current_version,
+              replacement: new_version
+            }
+          ],
+          usePrefix: false
+        },
+        files: [
+          {
+            expand: true,
+            src: [
+              'VERSION.md'
+            ]
+          }
+        ]
+      },
+      task2: {
+        options: {
+          patterns: [
+            {
+              match: 'git checkout ' + current_version,
+              replacement: 'git checkout ' + new_version
+            }
+          ],
+          usePrefix: false
+        },
+        files: [
+          {
+            expand: true,
+            src: [
+              'README.md'
+            ]
+          }
+        ]
+      },
+      task3: {
+        options: {
+          patterns: [
+            {
+              match: '"version": "' + current_version + '"',
+              replacement: '"version": "' + new_version + '"'
+            }
+          ],
+          usePrefix: false
+        },
+        files: [
+          {
+            expand: true,
+            src: [
+              'package.json'
+            ]
+          }
+        ]
+      },
+      task4: {
+        options: {
+          patterns: [
+            {
+              match: 'VERSION="' + current_version + '"',
+              replacement: 'VERSION="' + new_version + '"'
+            }
+          ],
+          usePrefix: false
+        },
+        files: [
+          {
+            expand: true,
+            src: [
+              'drupal-compose.sh'
+            ]
+          }
+        ]
+      }
+    }
+  });
+
+  grunt.registerTask('bump', 'replace');
+  grunt.registerTask('default', 'replace');
+};
