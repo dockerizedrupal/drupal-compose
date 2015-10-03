daba3f5461c38cf06242100f574f507270acd7cc
Jürgen Viljaste <j.viljaste@gmail.com>
2015-10-03 22:43:33 +0300

    Update

--- drupal-compose.sh
+++ drupal-compose.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-VERSION="1.1.1"
+VERSION="1.1.2"
 
 WORKING_DIR="$(pwd)"
 
@@ -66,6 +66,130 @@ docker_compose_file_path() {
   echo "${DOCKER_COMPOSE_FILE_PATH}"
 }
 
+general_docker_compose_template() {
+  local PROJECT_NAME="${1}"
+
+  cat <<EOF
+# Configuration generated with Drupal Compose version ${VERSION}
+apache:
+  extends:
+    file: host.yml
+    service: apache
+  image: dockerizedrupal/apache-2.4:1.1.1
+  hostname: apache
+  ports:
+    - "80"
+    - "443"
+  volumes_from:
+    - apache-data
+  links:
+    - php
+  environment:
+    - VHOST_PROJECT_NAME=${PROJECT_NAME}
+    - VHOST_PRIMARY_SERVICE=True
+    - VHOST_SERVICE_NAME=apache
+    - VHOST_VERSION_FILE_URL=https://raw.githubusercontent.com/dockerizedrupal/docker-apache-2.4/master/VERSION.md
+    - VHOST_REPOSITORY_URL=https://hub.docker.com/r/dockerizedrupal/apache-2.4/
+apache-data:
+  image: dockerizedrupal/data:1.1.0
+  hostname: apache-data
+  volumes:
+    - .:/apache/data
+mysql:
+  image: dockerizedrupal/mysql:1.1.0
+  hostname: mysql
+  volumes_from:
+    - mysql-data
+  environment:
+    - VHOST_PROJECT_NAME=${PROJECT_NAME}
+    - VHOST_SERVICE_NAME=mysql
+    - VHOST_VERSION_FILE_URL=https://raw.githubusercontent.com/dockerizedrupal/docker-mysql/master/VERSION.md
+    - VHOST_REPOSITORY_URL=https://hub.docker.com/r/dockerizedrupal/mysql/
+mysql-data:
+  image: dockerizedrupal/data:1.1.0
+  hostname: mysql-data
+  volumes:
+    - /mysql
+php:
+  extends:
+    file: host.yml
+    service: php
+  image: dockerizedrupal/php-5.6:1.1.0
+  hostname: php
+  volumes:
+    - ~/.ssh:/home/container/.ssh
+  volumes_from:
+    - apache-data
+  links:
+    - mysql
+    - mailcatcher:smtp
+    - memcached
+  environment:
+    - VHOST_PROJECT_NAME=${PROJECT_NAME}
+    - VHOST_SERVICE_NAME=php
+    - VHOST_VERSION_FILE_URL=https://raw.githubusercontent.com/dockerizedrupal/docker-php-5.6/master/VERSION.md
+    - VHOST_REPOSITORY_URL=https://hub.docker.com/r/dockerizedrupal/php-5.6/
+mailcatcher:
+  image: dockerizedrupal/mailcatcher:1.1.0
+  hostname: mailcatcher
+  ports:
+    - "80"
+    - "443"
+  environment:
+    - VHOST_PROJECT_NAME=${PROJECT_NAME}
+    - VHOST_SERVICE_NAME=mailcatcher
+    - VHOST_VERSION_FILE_URL=https://raw.githubusercontent.com/dockerizedrupal/docker-mailcatcher/master/VERSION.md
+    - VHOST_REPOSITORY_URL=https://hub.docker.com/r/dockerizedrupal/mailcatcher/
+phpmyadmin:
+  image: dockerizedrupal/phpmyadmin:1.1.1
+  hostname: phpmyadmin
+  ports:
+    - "80"
+    - "443"
+  links:
+    - mysql
+  environment:
+    - VHOST_PROJECT_NAME=${PROJECT_NAME}
+    - VHOST_SERVICE_NAME=phpmyadmin
+    - VHOST_VERSION_FILE_URL=https://raw.githubusercontent.com/dockerizedrupal/docker-phpmyadmin/master/VERSION.md
+    - VHOST_REPOSITORY_URL=https://hub.docker.com/r/dockerizedrupal/phpmyadmin/
+adminer:
+  image: dockerizedrupal/adminer:1.1.1
+  hostname: adminer
+  ports:
+    - "80"
+    - "443"
+  links:
+    - mysql
+  environment:
+    - VHOST_PROJECT_NAME=${PROJECT_NAME}
+    - VHOST_SERVICE_NAME=adminer
+    - VHOST_VERSION_FILE_URL=https://raw.githubusercontent.com/dockerizedrupal/docker-adminer/master/VERSION.md
+    - VHOST_REPOSITORY_URL=https://hub.docker.com/r/dockerizedrupal/adminer/
+memcached:
+  image: dockerizedrupal/memcached:1.1.0
+  hostname: memcached
+  environment:
+    - VHOST_PROJECT_NAME=${PROJECT_NAME}
+    - VHOST_SERVICE_NAME=memcached
+    - VHOST_VERSION_FILE_URL=https://raw.githubusercontent.com/dockerizedrupal/docker-memcached/master/VERSION.md
+    - VHOST_REPOSITORY_URL=https://hub.docker.com/r/dockerizedrupal/memcached/
+memcachephp:
+  image: dockerizedrupal/memcachephp:1.1.1
+  hostname: memcachephp
+  ports:
+    - "80"
+    - "443"
+  links:
+    - memcached
+  environment:
+    - VHOST_PROJECT_NAME=${PROJECT_NAME}
+    - VHOST_SERVICE_NAME=memcachedphp
+    - VHOST_VERSION_FILE_URL=https://raw.githubusercontent.com/dockerizedrupal/docker-memcachephp/master/VERSION.md
+    - VHOST_REPOSITORY_URL=https://hub.docker.com/r/dockerizedrupal/memcachephp/
+EOF
+}
+
 drupal_8_docker_compose_template() {
   local PROJECT_NAME="${1}"
 
@@ -89,7 +213,7 @@ apache:
     - VHOST_PRIMARY_SERVICE=True
     - VHOST_SERVICE_NAME=apache
     - VHOST_VERSION_FILE_URL=https://raw.githubusercontent.com/dockerizedrupal/docker-apache-2.4/master/VERSION.md
-    - VHOST_REPOSITORY_URL=https://hub.docker.com/r/dockerizedrupal/apache-2.2/
+    - VHOST_REPOSITORY_URL=https://hub.docker.com/r/dockerizedrupal/apache-2.4/
 apache-data:
   image: dockerizedrupal/data:1.1.0
   hostname: apache-data
@@ -540,30 +664,60 @@ if [ -n "${DRUPAL_ROOT}" ]; then
   read -p "drupal-compose: Enter project name: " PROJECT_NAME
 
   echo -n "$(drupal_8_docker_compose_template ${PROJECT_NAME})" > "${DRUPAL_ROOT}/${DOCKER_COMPOSE_FILE}"
-else
-  DRUPAL_ROOT="$(drupal_7_path)"
 
-  if [ -n "${DRUPAL_ROOT}" ]; then
-    read -p "drupal-compose: Enter project name: " PROJECT_NAME
+  echo "drupal-compose: ${DOCKER_COMPOSE_FILE} file has been created. Don't forget to add it to VCS alongside with Drupal."
 
-    echo -n "$(drupal_7_docker_compose_template ${PROJECT_NAME})" > "${DRUPAL_ROOT}/${DOCKER_COMPOSE_FILE}"
-  else
-    DRUPAL_ROOT="$(drupal_6_path)"
+  echo -n "$(host_docker_compose_template)" > "${DRUPAL_ROOT}/host.yml"
 
-    if [ -n "${DRUPAL_ROOT}" ]; then
-      read -p "drupal-compose: Enter project name: " PROJECT_NAME
+  echo "drupal-compose: host.yml file has been created. Please don't add it to VCS, since this file is specific to the host where it was generated."
 
-      echo -n "$(drupal_6_docker_compose_template ${PROJECT_NAME})" > "${DRUPAL_ROOT}/${DOCKER_COMPOSE_FILE}"
-    else
-      echo "drupal-compose: Drupal installation path could not be found."
-
-      exit 1
-    fi
-  fi
+  exit
 fi
+
+DRUPAL_ROOT="$(drupal_7_path)"
+
+if [ -n "${DRUPAL_ROOT}" ]; then
+  read -p "drupal-compose: Enter project name: " PROJECT_NAME
+
+  echo -n "$(drupal_7_docker_compose_template ${PROJECT_NAME})" > "${DRUPAL_ROOT}/${DOCKER_COMPOSE_FILE}"
+
+  echo "drupal-compose: ${DOCKER_COMPOSE_FILE} file has been created. Don't forget to add it to VCS alongside with Drupal."
+
+  echo -n "$(host_docker_compose_template)" > "${DRUPAL_ROOT}/host.yml"
+
+  echo "drupal-compose: host.yml file has been created. Please don't add it to VCS, since this file is specific to the host where it was generated."
+
+  exit
+fi
+
+DRUPAL_ROOT="$(drupal_6_path)"
+
+if [ -n "${DRUPAL_ROOT}" ]; then
+  read -p "drupal-compose: Enter project name: " PROJECT_NAME
+
+  echo -n "$(drupal_6_docker_compose_template ${PROJECT_NAME})" > "${DRUPAL_ROOT}/${DOCKER_COMPOSE_FILE}"
+
+  echo "drupal-compose: ${DOCKER_COMPOSE_FILE} file has been created. Don't forget to add it to VCS alongside with Drupal."
+
+  echo -n "$(host_docker_compose_template)" > "${DRUPAL_ROOT}/host.yml"
+
+  echo "drupal-compose: host.yml file has been created. Please don't add it to VCS, since this file is specific to the host where it was generated."
+
+  exit
+fi
+
+read -p "drupal-compose: Drupal installation path could not be found, would you like to create general purpose ${DOCKER_COMPOSE_FILE} file for your application anyways? [Y/n]: " ANSWER
+
+if [ "${ANSWER}" == "n" ]; then
+  exit
+fi
+
+read -p "drupal-compose: Enter project name: " PROJECT_NAME
+
+echo -n "$(general_docker_compose_template ${PROJECT_NAME})" > "${WORKING_DIR}/${DOCKER_COMPOSE_FILE}"
 
 echo "drupal-compose: ${DOCKER_COMPOSE_FILE} file has been created. Don't forget to add it to VCS alongside with Drupal."
 
-echo -n "$(host_docker_compose_template)" > "${DRUPAL_ROOT}/host.yml"
+echo -n "$(host_docker_compose_template)" > "${WORKING_DIR}/host.yml"
 
 echo "drupal-compose: host.yml file has been created. Please don't add it to VCS, since this file is specific to the host where it was generated."
