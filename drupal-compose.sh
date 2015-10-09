bbc9ba2ffceb8783c9d1996abfb304f89ea29eb4
Jürgen Viljaste <j.viljaste@gmail.com>
2015-10-10 01:01:34 +0300

    Update

--- drupal-compose.sh
+++ drupal-compose.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-VERSION="1.1.5"
+VERSION="1.1.6"
 
 WORKING_DIR="$(pwd)"
 
@@ -114,7 +114,7 @@ php:
   extends:
     file: host.yml
     service: php
-  image: dockerizedrupal/php-5.6:1.1.2
+  image: dockerizedrupal/php-5.6:1.1.3
   hostname: php
   volumes:
     - ~/.ssh:/home/container/.ssh
@@ -238,7 +238,7 @@ php:
   extends:
     file: host.yml
     service: php
-  image: dockerizedrupal/php-5.5:1.1.2
+  image: dockerizedrupal/php-5.5:1.1.3
   hostname: php
   volumes:
     - ~/.ssh:/home/container/.ssh
