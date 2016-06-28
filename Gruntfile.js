55b069e72fcd0f6b690395ad01e9084049ba082e
Jürgen Viljaste <j.viljaste@gmail.com>
2016-06-28 13:25:42 +0300

    #12

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,5 @@
-var current_version = '1.3.1';
-var new_version = '1.3.2';
+var current_version = '1.3.2';
+var new_version = '1.3.3';
 
 module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-replace');
--- drupal-compose.sh
+++ drupal-compose.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-VERSION="1.3.2"
+VERSION="1.3.3"
 
 WORKING_DIR="$(pwd)"
 
